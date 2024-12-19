0d90c947312a324e0e94fed602a24b4e4d1d3291
Bryanz28 <bryanz28@nycstudents.net>
2024-12-19 16:46:03 +0000

    push

--- src/main/java/com/example/project/Day3.java
+++ src/main/java/com/example/project/Day3.java
@@ -1,20 +1,28 @@
 package com.example.project;
 
 public class Day3 {
-      public static String[][] generateSnowflake(int size) { //you will be tested on this method
+      public static String[][] generateSnowflake(int size) { 
         String[][] grid = new String[size][size];
-
+        for(int i = 0;i<size;i++){
+          for(int j=0;j<size;j++){
+            if (i+j==size-1 || i==j||j==(size/2)||i==(size/2)) {
+              grid [i][j]="*";
+            } else {
+              grid [i][j]=" ";
+            }
+          }
+        }
     
 
         return grid;
     }
 
-    // Prints the snowflake  will be useful if tests fail (you will not be tested on this method)
+
     public static void printSnowflake(String[][] snowflake) {
         
     }
 
-    // Test for the snowflake generation
+
     public static void main(String[] args) {
     }
 }
--- src/main/java/com/example/project/Day4.java
+++ src/main/java/com/example/project/Day4.java
@@ -1,39 +1,80 @@
 package com.example.project;
+
 import java.util.Random;
 
 public class Day4 {
     
     private static String[] reindeer_names = {"Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"};
 
-    public static class Reindeer { // you must have at least a name, distance travelled, speed, run duration, and rest duration attribute
+    public static class Reindeer {
+        private String name;
+        private int speed;
+        private int runDuration;
+        private int restDuration;
+        private int distanceTraveled;
+        private int runTimeLeft;
+        private int restTimeLeft;
+        private boolean isRunning;
 
-
-        // Constructor 
+        // Constructor
         public Reindeer(String name, int speed, int runDuration, int restDuration) {
+            this.name = name;
+            this.speed = speed;
+            this.runDuration = runDuration;
+            this.restDuration = restDuration;
+            this.distanceTraveled = 0;
+            this.runTimeLeft = runDuration;
+            this.restTimeLeft = 0;
+            this.isRunning = true;
         }
 
-        public int getDistanceTraveled(){ // this method is required
-            return 0;
+        // Getter for distance traveled
+        public int getDistanceTraveled() {
+            return distanceTraveled;
         }
+
+        // Simulates a single second of running/resting
+        public void simulateSecond() {
+            if (isRunning) {
+                distanceTraveled += speed;
+                runTimeLeft--;
+                if (runTimeLeft == 0) {
+                    isRunning = false;
+                    restTimeLeft = restDuration;
+                }
+            } else {
+                restTimeLeft--;
+                if (restTimeLeft == 0) {
+                    isRunning = true;
+                    runTimeLeft = runDuration;
+                }
+            }
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static String simulateRace(int time, Reindeer[] reindeers) {
+        for (int t = 0; t < time; t++) {
+            for (Reindeer reindeer : reindeers) {
+                reindeer.simulateSecond();
+            }
+        }
+
+        // Find the reindeer that traveled the farthest
+        Reindeer winner = reindeers[0];
+        for (Reindeer reindeer : reindeers) {
+            if (reindeer.getDistanceTraveled() > winner.getDistanceTraveled()) {
+                winner = reindeer;
+            }
+        }
+
+        return winner.getName();
+    }
+
+    public static void main(String[] args) {
         
-
-        public void simulateSecond(){ //this method is required but not tested
-            
-        }
-
     }
-
-    public static String simulateRace(int time, Reindeer[] reindeers){ //you will be tested on this method
-        return "name of winner";
-    }
-
-    
-    public static void main(String[] args) { // for testing purposed
-
-    }
-
- 
 }
-
-
-
